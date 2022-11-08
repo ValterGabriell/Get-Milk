ac52ef839d69d73cbd7b09baccba3f6deaf52c2f
Valter Gabriel <63808405+ValterGabriell@users.noreply.github.com>
2022-11-08 17:50:28 -0300

    first commit com a tela login 80% pronta

---
+++ src/components/CustomInputText.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {TextInput, StyleSheet} from 'react-native';
+
+const CustomInput = ({value, setValue, placeholder, secureTextEntry}) => {
+    return (
+        <TextInput
+            value={value}
+            onChangeText={setValue}
+            placeholder={placeholder} 
+            style={estilos.contanier}
+            secureTextEntry={secureTextEntry}
+        />
+
+    );
+};
+
+const estilos = StyleSheet.create({
+    contanier:{
+        backgroundColor: "white",
+        width: "90%",
+        marginLeft:"4%",
+        marginBottom:"2%",
+        height: 55,
+        padding: 16,
+        fontWeight: '500',
+    },
+
+});
+
+export default CustomInput;
