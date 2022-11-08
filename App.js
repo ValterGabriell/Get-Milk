247a915e42f28c7fc03046bbbfa229acc2ad7bf2
Valter Gabriel <63808405+ValterGabriell@users.noreply.github.com>
2022-11-08 17:50:20 -0300

    first commit com a tela login 80% pronta

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import AppRoutes from './src/routes/AppRoutes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={{flex: 1}}>
       <StatusBar style="auto" />
-    </View>
+      <AppRoutes/>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
