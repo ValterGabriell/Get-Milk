6e2e2650e685d272f5d8adb5b98d190fb4fd5f95
Vicente-ferrer <vviicenteferreira@gmail.com>
2022-11-09 21:35:05 -0300

    adicionando tela de feed

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -2,29 +2,22 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import LoginScreen from "../screens/Login/index";
+import FeedScreen from "../screens/Feed/index";
+
 const Stack = createNativeStackNavigator();
 
 const AppRoutes = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login_Screen" component={LoginScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Login_Screen" component={FeedScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React from "react";
-import LoginScreen from "../screens/Login";
-const Stack = createNativeStackNavigator();
 
-const AppRoutes = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login_Screen" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+export default AppRoutes;
